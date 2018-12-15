3343adee5390649007cae721b86133e4a4f57ed9
susu <919304298@qq.com>
2018-12-15 20:48:56 +0800

    第三次修改-渲染数据

--- ress.js
+++ ress.js
@@ -1,4 +1,4 @@
-onst express = require('express');
+const express = require('express');
 const mongodb = require('mongodb');
 const bodyParser = require('body-parser');
 
@@ -6,18 +6,16 @@ const bodyParser = require('body-parser');
 const MongoClient = mongodb.MongoClient;
 //创建应用
 let app = express();
-//静态资源服务器
+//把当前目录当做静态资源服务器
 app.use(express.static('./'));
 
-//路由
-//注册
-// 路由
-// 注册
-let urlencodedParser = bodyParser.urlencoded({ extended: false })
-app.post('./priduct-category',urlencodedParser ,(req,res)=>{
-    let {username,password,gender} = req.body;
 
-    MongoClient.connect('mongodb://127.0.0.1::27017',(err, database)=>{
+// 路由
+// let urlencodedParser = bodyParser.urlencoded({ extended: false })
+app.get('/plist',(req,res)=>{
+
+
+    MongoClient.connect('mongodb://127.0.0.1:27017',(err, database)=>{
         //连接成功后执行这个回调函数
         if(err) throw err;
 
@@ -25,37 +23,21 @@ app.post('./priduct-category',urlencodedParser ,(req,res)=>{
         let db = database.db('usesu');
 
         // 使用集合
-        let user = db.collection('user');
+         let user = db.collection('user');
 
-
-        // 插入数据
-        user.find({}),(err,result)=>{
-            let data;
-            if(err){
-                data={
-                    code:0,
-                    data:[],
-                    msg:err
-                }
-            }else{
-                data = {
-                    code:1,
-                    data:result.ops,
-                    msg:'插入成功'
-                }
-            }
-
-            res.send(data);
+        user.find().toArray((err,result)=>{
+            console.log(result);
+            res.send(result);
         });
        
 
-        // 关闭数据库，避免资源浪费
         database.close();
 
     });
-})
+
+});
 
 
 app.listen(2333,()=>{
     console.log('server is running on http://localhost:2333');
-})
+});
